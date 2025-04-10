744f9cb4412d33bfb5774643207e3df88dc3e3a6
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2025-04-10 10:05:11 +0100

    Update nanobind to use tsl-robin-map 1.4.0. (#25)

--- recipes/nanobind/conanfile.py
+++ recipes/nanobind/conanfile.py
@@ -27,7 +27,7 @@ class NanobindConan(ConanFile):
         self.info.clear()
 
     def requirements(self):
-        self.requires("tsl-robin-map/tci-1.3.0@tket/stable")
+        self.requires("tsl-robin-map/tci-1.4.0@tket/stable")
 
     def source(self):
         get(
